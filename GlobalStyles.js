1e31fb6c5079e5525c2dc04b89064e9a6f928f2d
sniped-coder11 <cndevajit@gmail.com>
2024-06-10 09:54:11 +0530

    Final Commit of the Detection App UI

---
+++ GlobalStyles.js
@@ -0,0 +1,35 @@
+/* fonts */
+export const FontFamily = {
+  abhayaLibreSemiBold: "Abhaya Libre SemiBold",
+  adaminaRegular: "Adamina-Regular",
+  abhayaLibreBold: "AbhayaLibre-Bold",
+  assistantRegular: "Assistant-Regular",
+};
+/* font sizes */
+export const FontSize = {
+  size_11xl: 30,
+  size_23xl: 42,
+  size_5xs: 8,
+  size_mini: 15,
+  size_2xl: 21,
+};
+/* Colors */
+export const Color = {
+  colorWhite: "#fff",
+  colorGray_100: "rgba(255, 255, 255, 0.54)",
+  colorGray_200: "rgba(255, 255, 255, 0.85)",
+  colorDarkslateblue_100: "#112d63",
+  colorDarkslateblue_200: "rgba(8, 36, 92, 0.8)",
+  colorBlack: "#000",
+  colorSteelblue: "rgba(55, 78, 123, 0.4)",
+  colorPaleturquoise: "rgba(179, 236, 229, 0.87)",
+  colorGainsboro: "#d9d9d9",
+  colorDarkslategray: "#2a3439",
+};
+/* border radiuses */
+export const Border = {
+  br_9xs: 4,
+  br_6xl: 25,
+  br_5xs: 8,
+  br_3xs: 10,
+};
